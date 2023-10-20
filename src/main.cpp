3fa59d47bdc5eaa578e8e0b6f6656637ba55d466
hiroaki <hiroaki.tokyu8500@gmail.com>
2023-10-20 15:56:46 +0900

    送受信成功！

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 //-----------------------------------------------------------------
 
 #ifndef __IMXRT1062__
-  #error "This sketch should be compiled for Teensy 4.x"
+    #error "This sketch should be compiled for Teensy 4.x"
 #endif
 
 //-----------------------------------------------------------------
@@ -19,52 +19,54 @@
 //-----------------------------------------------------------------
 
 void setup () {
-  pinMode (LED_BUILTIN, OUTPUT) ;
-  Serial.begin (9600) ;
-  while (!Serial) {
-    delay (50) ;
-    digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
-  }
-  Serial.println ("CAN1 loopback test") ;
-  ACAN_T4_Settings settings (125 * 1000) ; // 125 kbit/s
-  settings.mLoopBackMode = true ;
-  settings.mSelfReceptionMode = true ;
-  settings.mReceiveBufferSize = 32;
-  const uint32_t errorCode = ACAN_T4::can1.begin (settings) ;
-  Serial.print ("Bitrate prescaler: ") ;
-  Serial.println (settings.mBitRatePrescaler) ;
-  Serial.print ("Propagation Segment: ") ;
-  Serial.println (settings.mPropagationSegment) ;
-  Serial.print ("Phase segment 1: ") ;
-  Serial.println (settings.mPhaseSegment1) ;
-  Serial.print ("Phase segment 2: ") ;
-  Serial.println (settings.mPhaseSegment2) ;
-  Serial.print ("RJW: ") ;
-  Serial.println (settings.mRJW) ;
-  Serial.print ("Triple Sampling: ") ;
-  Serial.println (settings.mTripleSampling ? "yes" : "no") ;
-  Serial.print ("Actual bitrate: ") ;
-  Serial.print (settings.actualBitRate ()) ;
-  Serial.println (" bit/s") ;
-  Serial.print ("Exact bitrate ? ") ;
-  Serial.println (settings.exactBitRate () ? "yes" : "no") ;
-  Serial.print ("Distance from wished bitrate: ") ;
-  Serial.print (settings.ppmFromWishedBitRate ()) ;
-  Serial.println (" ppm") ;
-  Serial.print ("Sample point: ") ;
-  Serial.print (settings.samplePointFromBitStart ()) ;
-  Serial.println ("%") ;
-  if (0 == errorCode) {
-    Serial.println ("can1 ok") ;
-  }else{
-    Serial.print ("Error can1: 0x") ;
-    Serial.println (errorCode, HEX) ;
-    while (1) {
-      delay (100) ;
-      Serial.println ("Invalid setting") ;
-      digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
+    pinMode (LED_BUILTIN, OUTPUT) ;
+    Serial.begin (9600) ;
+    while (!Serial) {
+        delay (50) ;
+        digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
     }
-  }
+    Serial.println ("CAN1 - CAN2 loopback test") ;
+    ACAN_T4_Settings settings (125 * 1000) ; // 125 kbit/s
+    // settings.mLoopBackMode = true ;
+    // settings.mSelfReceptionMode = true ;
+    settings.mReceiveBufferSize = 4;
+    settings.mTransmitBufferSize = 64;
+    const uint32_t errorCode = ACAN_T4::can1.begin (settings) ;
+    const uint32_t errorCode2 = ACAN_T4::can2.begin(settings) ;
+    Serial.print ("Bitrate prescaler: ") ;
+    Serial.println (settings.mBitRatePrescaler) ;
+    Serial.print ("Propagation Segment: ") ;
+    Serial.println (settings.mPropagationSegment) ;
+    Serial.print ("Phase segment 1: ") ;
+    Serial.println (settings.mPhaseSegment1) ;
+    Serial.print ("Phase segment 2: ") ;
+    Serial.println (settings.mPhaseSegment2) ;
+    Serial.print ("RJW: ") ;
+    Serial.println (settings.mRJW) ;
+    Serial.print ("Triple Sampling: ") ;
+    Serial.println (settings.mTripleSampling ? "yes" : "no") ;
+    Serial.print ("Actual bitrate: ") ;
+    Serial.print (settings.actualBitRate ()) ;
+    Serial.println (" bit/s") ;
+    Serial.print ("Exact bitrate ? ") ;
+    Serial.println (settings.exactBitRate () ? "yes" : "no") ;
+    Serial.print ("Distance from wished bitrate: ") ;
+    Serial.print (settings.ppmFromWishedBitRate ()) ;
+    Serial.println (" ppm") ;
+    Serial.print ("Sample point: ") ;
+    Serial.print (settings.samplePointFromBitStart ()) ;
+    Serial.println ("%") ;
+    if (0 == errorCode) {
+        Serial.println ("can1 ok") ;
+    }else{
+        Serial.print ("Error can1: 0x") ;
+        Serial.println (errorCode, HEX) ;
+        while (1) {
+        delay (100) ;
+        Serial.println ("Invalid setting") ;
+        digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
+        }
+    }
 }
 
 //-----------------------------------------------------------------
@@ -73,28 +75,51 @@ static uint32_t gBlinkDate = 0 ;
 static uint32_t gSendDate = 0 ;
 static uint32_t gSentCount = 0 ;
 static uint32_t gReceivedCount = 0 ;
+bool programEnd = false;
 
+uint64_t cnt = 0;
 //-----------------------------------------------------------------
 
 void loop () {
-  if (gBlinkDate <= millis ()) {
-    gBlinkDate += 500 ;
-    digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
-  }
-  CANMessage message ;
-  if (gSendDate <= millis ()) {
-    message.id = 0x542 ;
-    const bool ok = ACAN_T4::can1.tryToSend (message) ;
-    if (ok) {
-      gSendDate += 2000 ;
-      gSentCount += 1 ;
-      Serial.print ("Sent: ") ;
-      Serial.println (gSentCount) ;
+    if (gBlinkDate <= millis ()) {
+        gBlinkDate += 500 ;
+        digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
     }
-  }
-  if (ACAN_T4::can1.receive (message)) {
-    gReceivedCount += 1 ;
-    Serial.print ("Received: ") ;
-    Serial.println (gReceivedCount) ;
-  }
+    CANMessage message ;
+    if (! programEnd){
+        // if (gSendDate <= millis ()) {
+        //     while(true){
+        //         message.id = 0x542 ;
+        //         message.data64 = cnt;
+        //         const bool ok = ACAN_T4::can1.tryToSend (message) ;
+        //         if (ok) {
+        //             Serial.print ("Sent: ") ;
+        //             Serial.println (cnt) ;
+        //         }
+        //         if (cnt == 63){
+        //             Serial.println("SEND END");
+        //             break;
+        //         }
+        //         cnt++ ;
+        //     }
+        // }
+        Serial.println("READ START");
+        while(true){
+            if (ACAN_T4::can2.receive (message)) {
+                gReceivedCount += 1 ;
+                Serial.print ("Received: ") ;
+                int tmp = message.data64;
+                Serial.println (tmp) ;
+                Serial.print("count is ");
+                Serial.println(gReceivedCount);
+
+            }
+            if (gReceivedCount == 63){
+                Serial.println("READ END");
+                programEnd = true;
+                break;
+            }
+        }
+    }
+
 }
